242826f383a703ce8e625f225a2ec4cb587fc658
Niels van Beusekom <nvbeusekom@pm.me>
2025-09-10 01:50:26 +0200

    Add biome color rendering with interpolation

---
+++ render/__init__.py
@@ -0,0 +1,5 @@
+"""Rendering utilities for terrain maps."""
+
+from .biome_colors import BIOME_BASE_COLORS, biome_color_map, bilinear_interpolate
+
+__all__ = ["BIOME_BASE_COLORS", "biome_color_map", "bilinear_interpolate"]
